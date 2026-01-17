2f13a8085f86da614b2e10e05f72f08e51390832
Prince Kumar <prince.infoera@gmail.com>
2026-01-17 12:14:05 +0530

    Project initillized

---
+++ src/main/java/com/infoera/sportshub_inventory/SportshubInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.infoera.sportshub_inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportshubInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportshubInventoryApplication.class, args);
+	}
+
+}
